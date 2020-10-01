419fcc29845502a02f7f9b88e9cda48879bb7530
monalice <alice.amorim@engenharia.ufjf.br>
2020-10-01 17:55:16 -0300

    Layout estatico

--- src/main.js
+++ src/main.js
@@ -1,1 +1,6 @@
 //comportamento do menu
+var API_KEY = `00677d2daa69a1cc4505e8c461dd2031`;
+
+import renderPopular from "./popular.js";
+
+renderPopular();
--- src/popular.js
+++ src/popular.js
@@ -1,1 +1,10 @@
 //Renderiza lista de filmes populares
+
+function requestPopular() {
+    var req = axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=00677d2daa69a1cc4505e8c461dd2031`);
+    req.then(renderPopular);
+}
+
+export default function renderPopular(response) {
+    console.log(response);
+}
